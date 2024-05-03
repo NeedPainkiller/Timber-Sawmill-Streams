44716b57bfc01250b8590847f99d659f07524bc7
needpainkiller <needpainkiller6512@gmail.com>
2024-05-04 02:13:48 +0900

    Add : Application.java

---
+++ src/main/java/xyz/needpankiller/timber/sawmill/streams/Application.java
@@ -0,0 +1,8 @@
+package xyz.needpankiller.timber.sawmill.streams;
+
+import jakarta.enterprise.context.ApplicationScoped;;
+
+@ApplicationScoped
+public class Application extends jakarta.ws.rs.core.Application {
+
+}
